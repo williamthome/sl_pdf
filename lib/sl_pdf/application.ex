bd9086977700e4e273bfdc172b7b4e47ccb6dec1
William Fank Thome <willegp88@gmail.com>
2022-03-08 19:57:47 -0300

    feat: generate pdf

--- lib/sl_pdf/application.ex
+++ lib/sl_pdf/application.ex
@@ -8,8 +8,10 @@ defmodule SlPdf.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: SlPdf.Worker.start_link(arg)
-      # {SlPdf.Worker, arg}
+      # Start the ChromicPDF supervisor
+      ChromicPDF,
+      # Pdf supervisor
+      SlPdf.PdfSup
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
