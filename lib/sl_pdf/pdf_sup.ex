670d1cf0b5d0c0c3c69ebd113c33fa974da201de
William Fank Thome <willegp88@gmail.com>
2022-03-08 21:20:34 -0300

    refactor(PdfSup): set start_link default value

--- lib/sl_pdf/pdf_sup.ex
+++ lib/sl_pdf/pdf_sup.ex
@@ -1,7 +1,7 @@
 defmodule SlPdf.PdfSup do
   use Supervisor
 
-  def start_link(init_arg) do
+  def start_link(init_arg \\ []) do
     Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
   end
 
