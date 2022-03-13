b1b198709c67bb345763ee4c1da3c3abbc9b433b
William Fank Thome <willegp88@gmail.com>
2022-03-13 11:25:33 -0300

    feat: generic test

--- lib/sl_pdf.ex
+++ lib/sl_pdf.ex
@@ -17,6 +17,11 @@ defmodule SlPdf do
     opts \\ []
   ), do: GenServer.call(__MODULE__, {:print_to_pdf, content, callback, opts}, @print_timeout)
 
+  def test(
+    n_columns \\ 10,
+    n_rows \\ 500
+  ), do: GenServer.call(__MODULE__, {:test, n_columns, n_rows}, @print_timeout)
+
   ## Callbacks
 
   @impl true
@@ -26,5 +31,14 @@ defmodule SlPdf do
   def handle_call({:print_to_pdf, content, callback, opts}, _from, state) do
     pdf = content |> PdfView.print(callback, opts)
     {:reply, pdf, state}
+  end
+
+  @impl true
+  def handle_call({:test, n_columns, n_rows}, _from, state) do
+    {microseconds, pdf_result} = :timer.tc(fn ->
+      PdfView.test(n_columns, n_rows)
+    end)
+    seconds = microseconds / 1_000_000
+    {:reply, {pdf_result, {:seconds, seconds}}, state}
   end
 end
--- lib/sl_pdf_web/views/pdf_view.ex
+++ lib/sl_pdf_web/views/pdf_view.ex
@@ -2,7 +2,15 @@ defmodule SlPdfWeb.PdfView do
   @templates [
     {:hello_world, "hello_world.html", []},
     {:greeting, "greeting.html", [:name]},
-    {:loop, "loop.html", [:list]}
+    {:loop, "loop.html", [:list]},
+    {:hard_work, "hard_work.html", [:columns, :rows]}
   ]
   use SlPdfWeb, :pdf_view
+
+  def test(n_columns, n_rows) do
+    columns = for column <- 1..n_columns, do: "Column#{column}"
+    rows = for row <- 1..n_rows, do: for column <- 1..n_columns, do: "Row#{row}Column#{column}"
+
+    print(hard_work(columns, rows), "hard_work.pdf")
+  end
 end
