a585931fbe7ab6ce231db3a683db0a3d253f3fc0
William Fank Thome <willegp88@gmail.com>
2022-03-15 07:46:23 -0300

    fix: macro is not supported at config

--- config/config.exs
+++ config/config.exs
@@ -2,15 +2,17 @@ import Config
 
 # TODO: Split configs by env in multiple files
 
-@chromic_pdf_common_env_config [
-  {:offline, true},
-  {:on_demand, true},
-  {:session_pool, [{:timeout, 60_000}]}
-]
-
 case config_env() do
   :prod ->
-    config :sl_report, ChromicPDF, @chromic_pdf_common_env_config
+    config :sl_report, ChromicPDF, [
+      {:offline, true},
+      {:on_demand, true},
+      {:session_pool, [{:timeout, 60_000}]}
+    ]
   _env ->
-    config :sl_report, ChromicPDF, @chromic_pdf_common_env_config
+    config :sl_report, ChromicPDF, [
+      {:offline, true},
+      {:on_demand, true},
+      {:session_pool, [{:timeout, 60_000}]}
+    ]
 end
