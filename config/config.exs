1fceb654a83e24bb17dacf8af5598c4c3112df6b
William Fank Thome <willegp88@gmail.com>
2022-03-15 07:41:57 -0300

    refactor: use on_demand for prod due to concurrency issues

--- config/config.exs
+++ config/config.exs
@@ -2,16 +2,15 @@ import Config
 
 # TODO: Split configs by env in multiple files
 
+@chromic_pdf_common_env_config [
+  {:offline, true},
+  {:on_demand, true},
+  {:session_pool, [{:timeout, 60_000}]}
+]
+
 case config_env() do
   :prod ->
-    config :sl_report, ChromicPDF, [
-      {:on_demand, false},
-      {:session_pool, [{:timeout, 60_000}]}
-    ]
-  _ ->
-    config :sl_report, ChromicPDF, [
-      {:offline, true},
-      {:on_demand, true},
-      {:session_pool, [{:timeout, 60_000}]}
-    ]
+    config :sl_report, ChromicPDF, @chromic_pdf_common_env_config
+  _env ->
+    config :sl_report, ChromicPDF, @chromic_pdf_common_env_config
 end
